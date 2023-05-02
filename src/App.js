5609221563fc454e9d5a872ae9b3ace032fd5a9c
Kevin <kevin.rodriguez678@gmail.com>
2023-05-02 19:20:51 -0400

    create component folder to incorporate custom component buttons

--- src/App.js
+++ src/App.js
@@ -10,11 +10,20 @@ const ACTIONS = {
 }
 
 function reducer(state, {type, payload}) {
-
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${currentOperand || ""}${payload.digit}`,
+      }
+  }
 }
 
 function App() {
   const [{currentoperand, previousoperand, operation}, dispatch] = useReducer(reducer, {})
+
+  dispatch({type: ACTIONS.ADD_DIGIT, payload: {digit: 1}})
+
   return (
     <div className="calculator-grid">
       <div className='output'>
